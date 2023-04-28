c0aaede7df470e75ae96f432323fbfe9bff82479
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-04-28 14:43:38 -0300

    Implementando a verificacao de xeque

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -21,6 +21,11 @@
             QtdMovimentos++;
         }
 
+        public void DecrementarQtdMovimentos()
+        {
+            QtdMovimentos--;
+        }
+
         public bool ExisteMovimentosPossiveis()
         {
             bool[,] mat = MovimentosPossiveis();
